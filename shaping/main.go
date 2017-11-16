31cfcea7e2e4b11da94471aa2a6a616e4705fc78
Marwan Rabbâa <waghanza@gmail.com>
2017-11-16 22:28:29 +0100

    start writing github compatible method

--- shaping/main.go
+++ shaping/main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"net/http"
 	"github.com/labstack/echo"
+	"gopkg.in/rjz/githubhook.v0"
+	"flag"
 )
 
-func helloWorld(c echo.Context) error {
-
-	return c.String(http.StatusOK, "Hello, World!");
-
+func githubWebHook(c echo.Context) error {
+	key := flag.String("key", "not secured", "github token")
+	flag.Parse()
+	secret := []byte(*key);
+	_, err := githubhook.Parse(secret, c.Request());
+	return err;
 }
 
 func main() {
 
+
 	// Initialize echo
-	e := echo.New()
+e := echo.New()
 
-	// Route definition
-	e.GET("/", helloWorld);
+	   // Route definition
+	   e.POST("/github", githubWebHook);
 
-	// Start server
-	e.Start("127.0.0.1:3000");
+   // Start server
+   e.Start("127.0.0.1:3000");
 }
